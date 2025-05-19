21823c02237e22746f52aa36e0313459c1f73e8f
43Daniil <cg0mlq@gmail.com>
2025-05-19 18:31:02 +0200

    Update main19.c

--- K&R13-24/main19.c
+++ K&R13-24/main19.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 
+int reve3(char s[], int length);
+
+int main()
+{
+    int length;
+    char str1[1000];
+    fgets(str1, sizeof(str1), stdin);
+    length = strlen(str1);
+    reve3(str1, length);
+    return 0;
+}
+int reve3(char s[], int length)
+{
+    for(int i = length; i >= 0; i--)
+    {
+        printf("%c", s[i]);
+    }
+    return 0;
+}
