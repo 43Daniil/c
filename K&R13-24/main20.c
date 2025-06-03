75a2f1deeef38015a8eaf3a889191f918b9af3d5
43Daniil <cg0mlq@gmail.com>
2025-06-03 19:17:35 +0200

    Update main20.c

--- K&R13-24/main20.c
+++ K&R13-24/main20.c
@@ -1,1 +1,29 @@
-
+#include <stdio.h>
+#define TABSTOP 8
+int main()
+{
+    int tabs = 0;
+    int c;
+    int length = 0;
+    while((c = getchar())!= EOF)
+    {
+        if (c == '\t')
+        {
+            tabs = TABSTOP-length;
+            for(int i = 0; i < tabs; i++)
+            {
+                printf(" ");
+                length++;
+            }
+            continue;
+        }
+        length++;
+        if (c == '\n')
+        {
+            length = 0;
+            tabs = 0;
+        }
+        putchar(c);
+    }
+    return 0;
+}
